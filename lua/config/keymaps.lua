d294c68361c3cded3b18ab5e89fff3c5cd5ec7c6
Stefan Milovanovic <dazmaj@hotmail.com>
2023-05-04 22:55:44 +0200

    feat: upgrade deps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- prevent cut
+vim.keymap.set("n", "C", '"_C')
+vim.keymap.set("n", "c", '"_c')
+vim.keymap.set("v", "c", '"_c')
+vim.keymap.set("n", "<leader>d", '"_d')
+vim.keymap.set("n", "<leader>D", '"_D')
+vim.keymap.set("v", "<leader>d", '"_d')

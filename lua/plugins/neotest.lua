639cf2fe5c3faf0bde85d8ef2be2688ec8752621
Stefan Milovanovic <stefan@192.168.1.8>
2023-08-29 15:06:32 +0200

    feat: env files detect

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -4,7 +4,13 @@ return {
     "nvim-neotest/neotest",
     dependencies = {
       "olimorris/neotest-rspec",
+      "haydenmeade/neotest-jest"
     },
-    opts = { adapters = { "neotest-rspec" } },
+    opts = {
+      adapters = { "neotest-rspec", "neotest-jest" },
+      output_panel = {
+        open = "botright vsplit | vertical resize 100",
+      }
+    },
   },
 }

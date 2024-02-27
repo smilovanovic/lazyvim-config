5d0d13b35b48ba0ec4ab0872d624ceb2d72d6902
Stefan Milovanovic <stefan@192.168.1.7>
2024-02-27 13:44:19 +0100

    feat: upgrade and themes remove

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,18 +7,6 @@ return {
       dim_inactive = true,
     },
   },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    opts = {
-      dim_inactive = {
-        enabled = true, -- dims the background color of inactive window
-        shade = "light",
-        percentage = 0.75,
-      },
-    },
-  },
-
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
